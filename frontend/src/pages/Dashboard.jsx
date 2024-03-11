4affad401ccd42ee35c0cdb621bc4b3a95577996
ziaurrehman07 <ziaurrehman939@gmail.com>
2024-03-12 00:50:27 +0530

    set navbar width to 95%

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -8,7 +8,7 @@ function Dashboard() {
         <h1 className="ml-8 mt-8 text-blue-500 font-semibold text-xl">
           User Dashboard
         </h1>
-        <div className=" ml-8 mt-4 bg-white h-[75%] w-[90%] rounded-lg shadow-xl "></div>
+        <div className=" ml-4 mt-4 bg-white h-[75%] w-[95%] rounded-lg shadow-xl "></div>
       </div>
     </>
   );
