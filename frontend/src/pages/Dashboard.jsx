c621dfa0eeff84d245c32ce6462c95441201c7cc
ziaurrehman07 <ziaurrehman939@gmail.com>
2024-03-13 01:18:45 +0530

    dashboard update

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -3,12 +3,14 @@ import Navbar from "../components/Navbar";
 function Dashboard() {
   return (
     <>
-      <div className="flex w-full flex-col bg-[#f5f5f5]">
+      <div className="flex w-full flex-col h-screen bg-[#e9f1ef]">
         <Navbar />
-        <h1 className="ml-8 mt-8 text-blue-500 font-semibold text-xl">
+        <h1 className="ml-8 mt-8 text-blue-500 font-semibold text-xl ">
           User Dashboard
         </h1>
-        <div className=" ml-8 mt-4 bg-white h-[75%] w-[93%] rounded-lg shadow-xl "></div>
+        <div className=" ml-8 mt-4 bg-white h-[75%] w-[93%] rounded-lg shadow-xl overflow-y-scroll no-scrollbar">
+          <div className="p-8"></div>
+        </div>
       </div>
     </>
   );
