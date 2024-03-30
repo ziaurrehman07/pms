0bf1dfb192b0dcd735fc15ee9dcb3eda2545b403
faijanbaig <faijanbaig2308@gmail.com>
2024-03-31 00:21:55 +0530

    "Add getAllFeedbacks function in feedback controller and corresponding route in feedback route"

--- backend/src/routes/feedback.route.js
+++ backend/src/routes/feedback.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { verifyJWT, verifyAdmin } from "../middlewares/auth.middleware.js";
-import { newFeedback } from "../controllers/feedback.controller.js";
+import { getAllFeedbacks, newFeedback } from "../controllers/feedback.controller.js";
 
 const router = Router();
 
 router.route("/write-new-feedback").post(verifyJWT, newFeedback);
+router.route("/get-all-feedbacks").get(verifyAdmin, getAllFeedbacks);
 
 export default router;
