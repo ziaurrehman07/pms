299554843229bee0ac7f9624aa38dc1be304827a
faijanbaig <faijanbaig2308@gmail.com>
2024-04-02 16:49:13 +0530

    Add routes and controllers for company to get all jobs and job details

--- backend/src/routes/job.route.js
+++ backend/src/routes/job.route.js
@@ -1,6 +1,6 @@
 import {Router} from "express"
 import {verifyJWT, verifyJwtForCompany} from "../middlewares/auth.middleware.js"
-import { applyForJob, deleteJobProfile, getAllJobProfile, getJobDetailsById, newJobProfile, updateJobProfile } from "../controllers/job.controller.js"
+import { applyForJob, deleteJobProfile, getAllJobProfile, getCompanyAllJobs, getCompanyJobDetailsById, getJobDetailsById, newJobProfile, updateJobProfile } from "../controllers/job.controller.js"
 
 const router = Router()
 
@@ -11,5 +11,8 @@ router.route("/delete-job-profile/:jobId").delete(verifyJwtForCompany,deleteJobP
 router.route("/apply-for-job/:jobId").get(verifyJWT,applyForJob)
 router.route("/get-all-jobs").get(verifyJWT,getAllJobProfile)
 router.route("/get-job-details/:jobId").get(verifyJWT,getJobDetailsById)
+router.route("/get-current-company-all-jobs").get(verifyJwtForCompany,getCompanyAllJobs)
+router.route("/get-current-company-job-details/:jobId").get(verifyJwtForCompany,getCompanyJobDetailsById)
+
 
 export  default router;
