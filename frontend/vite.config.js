1dd88f90433b33604a7aa2224888d306a268d940
ziaurrehman07 <ziaurrehman939@gmail.com>
2024-04-22 21:59:22 +0530

    update vite..

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://pmsbackend-wzvr.onrender.com/",
-        // secure: false,
+        secure: false,
         changeOrigin: true,
       },
     },
