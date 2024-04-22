4e603c2f0f6fa0abde6f509ba3c6584d80825b12
ziaurrehman07 <ziaurrehman939@gmail.com>
2024-04-22 22:47:07 +0530

    update vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://pmsbackend-wzvr.onrender.com/",
+        target: "https://pmsbackend-wzvr.onrender.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
