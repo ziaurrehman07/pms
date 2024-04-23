30ce5837efbcaa165e9505f13d2b35f71ca31482
ziaurrehman07 <ziaurrehman939@gmail.com>
2024-04-24 00:00:04 +0530

    vite updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "https://pmsfrontend-563q.onrender.com",
   server: {
     proxy: {
       "/api": {
-        target: "https://pmsbackend-wzvr.onrender.com",
+        target: "process.env.TARGET_URL",
+        secure: false,
         changeOrigin: true,
       },
     },
