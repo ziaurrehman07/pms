851dafe5eff52caf0c158366952d29684e80b6b7
ziaurrehman07 <ziaurrehman939@gmail.com>
2024-09-02 14:07:23 +0530

    cors error checking

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://pmsbackend-4lvq.onrender.com",
-        secure: false,
-        changeOrigin: true,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "https://pmsbackend-4lvq.onrender.com",
+  //       secure: false,
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
   plugins: [react()],
 });
