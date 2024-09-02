19e6c6cf17ea479f1529d5e8e5461c5bf252429d
ziaurrehman07 <ziaurrehman939@gmail.com>
2024-09-02 16:09:22 +0530

    index.js error fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,17 +1,9 @@
 import cookieParser from "cookie-parser";
 import express from "express";
-import cors from "cors";
 
 const app = express();
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    Credential: true,
-    methods: "GET,POST,PUT,DELETE", // Allowed HTTP methods
-  })
-);
 app.use(express.json({ limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
