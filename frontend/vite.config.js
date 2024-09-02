7334b975f407393ae5274c2f0898c59a15238b11
ziaurrehman07 <ziaurrehman939@gmail.com>
2024-09-02 19:25:13 +0530

    nb

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://pmsservice.onrender.com",
-        secure: false,
-        changeOrigin: true,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "https://pmsservice.onrender.com",
+  //       secure: false,
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
   plugins: [react()],
 });
