e3fb1f406f3b0cdad684da51789e13247a6e132a
ziaurrehman07 <ziaurrehman939@gmail.com>
2024-09-02 19:38:02 +0530

    cors updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(
   cors({
-    origin: "https://pms-s6t8.onrender.com",
+    origin: process.env.CORS_ORIGIN || "https://localhost:5173",
     credentials: true,
     methods: "GET,POST,PUT,DELETE", // Allowed HTTP methods
   })
