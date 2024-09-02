61fb1c295e6657695cb3bbf314c5f07364ffd119
ziaurrehman07 <ziaurrehman939@gmail.com>
2024-09-02 21:54:02 +0530

    NB

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,15 +3,15 @@ import express from "express";
 import cors from "cors";
 
 const app = express();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN || "https://localhost:5173",
+    credentials: true,
+    methods: "GET,POST,PUT,DELETE, PATCH",
+  })
+);
+
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
-// app.use(
-//   cors({
-//     origin: process.env.CORS_ORIGIN || "https://localhost:5173",
-//     // credentials: true,
-//     methods: "GET,POST,PUT,DELETE", // Allowed HTTP methods
-//   })
-// );
-app.use(cors());
 app.use(express.json({ limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
