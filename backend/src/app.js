dc91b5c6a83a19c567b4ac9e5b62676611849cb8
ziaurrehman07 <ziaurrehman939@gmail.com>
2024-09-03 17:38:49 +0530

    https remove add http to localhos

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ import cors from "cors";
 const app = express();
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "https://localhost:5173",
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
     credentials: true,
     methods: "GET,POST,PUT,DELETE, PATCH",
   })
