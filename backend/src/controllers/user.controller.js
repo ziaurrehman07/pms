6eace4c2389ce1a649a10628d675f3075e82742b
ziaurrehman07 <111639865+ziaurrehman07@users.noreply.github.com>
2024-09-07 03:35:55 +0530

    Update user.controller.js

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -110,7 +110,7 @@ const loginUser = asyncHandler(async (req, res) => {
   );
 
   const options = {
-    httpOnly: true,
+    httpOnly: false,
     secure: true,
     // domain: "https://pms-s6t8.onrender.com",
   };
