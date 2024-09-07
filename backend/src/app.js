3f7f58b024d8e6b24662774982c15e5dd4d7afbc
ziaurrehman07 <ziaurrehman939@gmail.com>
2024-09-07 14:55:58 +0530

    ff

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,17 +4,16 @@ import cors from "cors";
 import bodyParser from "body-parser";
 
 const app = express();
-app.use(cookieParser());
-app.use(bodyParser.json());
-app.use(express.json());
-
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(express.json());
 
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
