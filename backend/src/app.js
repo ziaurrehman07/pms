e1f0200c5328e39da39c0186e8a3dd00eae455dc
ziaurrehman07 <ziaurrehman939@gmail.com>
2024-09-07 14:40:38 +0530

    backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,20 +8,14 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(express.json());
 
-// app.use(
-//   cors({
-//     origin: process.env.CORS_ORIGIN || "http://localhost:5173",
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//   })
-// );
 app.use(
   cors({
-    origin: "https://pms-s6t8.onrender.com",
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
+
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 
