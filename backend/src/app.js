2ff8723d455aa5934257fcbdc5cd353bb1fca3ca
ziaurrehman07 <ziaurrehman939@gmail.com>
2024-09-07 12:47:17 +0530

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,18 +5,24 @@ import bodyParser from "body-parser";
 
 const app = express();
 app.use(cookieParser());
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+// app.use(
+//   cors({
+//     origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//   })
+// );
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 // app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.json());
 // app.use(express.static("public"));
-
 
 // Routes import
 import userRouter from "../src/routes/user.route.js";
