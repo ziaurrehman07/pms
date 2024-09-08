a2b19bf6ac527c97c9e2fafa1f79ed3a2629b742
ziaurrehman07 <ziaurrehman939@gmail.com>
2024-09-09 02:47:19 +0530

    in backend add patch methods in cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,7 +18,7 @@ app.use(
   cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
 );
 
