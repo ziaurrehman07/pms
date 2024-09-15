a8a41c176e216742132bb0b5641cec77f6a7e259
faijanbaig <faijanbaig2308@gmail.com>
2024-09-15 23:16:57 +0530

    cors fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,7 +16,7 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
