3a2e7e878ede5d176cc209adb3203737a93d6316
ziaurrehman07 <ziaurrehman939@gmail.com>
2024-09-23 20:37:42 +0530

    remove redux

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,19 +1,17 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { store } from "./redux/store.js";
-import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <Provider store={store}>
+  <>
     <App />
     <ToastContainer
-      position="top-center" // Position at the center
-      autoClose={3000} // Close after 3 seconds
-      hideProgressBar={true} // Hide progress bar
-      toastStyle={{ fontSize: "14px" }} // Set font size
-      bodyClassName="text-sm" // Set smaller text size
+      position="top-center"
+      autoClose={3000}
+      hideProgressBar={true}
+      toastStyle={{ fontSize: "14px" }}
+      bodyClassName="text-sm"
     />
-  </Provider>
+  </>
 );
--- frontend/src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./user/userSlice";
-
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-});
